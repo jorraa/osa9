99cf43a9acbd710b09004d71f1b610226b87d77c
Jorma Raatikainen <raatikainen.jorma@gmail.com>
2020-05-16 13:08:37 +0300

    9.1 tehty

---
+++ ts1/bmiCalculator.ts
@@ -0,0 +1,38 @@
+interface BmiValues {
+  weight: number;
+  height: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      weight: Number(args[2]),
+      height: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+const getBmiText = (bmi: number) => {
+  return bmi < 18.5 
+    ?'underweight'
+    : !(bmi > 25)
+      ?'Normal (healthy weight)'
+      :bmi > 30 
+        ?'obese'
+        :'overweight' 
+}
+
+const bmiCalculator = (weight: number, height: number) : number => {
+  return weight / Math.pow(height/100,2)
+}
+
+try {
+  const { weight, height } = parseArguments(process.argv);
+  console.log(getBmiText( bmiCalculator(weight, height)));
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
